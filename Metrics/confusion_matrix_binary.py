acbdee2abc2839ef40bf0b4887b9a8800f8cd748
Sathya Krishnan <86184014+SathyaKrishnan1211@users.noreply.github.com>
2022-03-21 20:22:05 +0530

    updated version of binary confusion matrix

---
+++ Metrics/confusion_matrix_binary.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def confusion_matrix_binary(y_true,y_pred):
+    """
+    Confusion matrix is an evaluation metric that is used to output the values of true positive and false negative (row wise).
+    In this function the actual classes come along the rows.
+    :param y_true: true class labels
+    :param y_pred: predicted class labels
+    :return: Returns the unique classes which points out the row-order of confusion matrix and the confusion matrix
+    """
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+
+    if len(y_true)!=len(y_pred):
+        raise ValueError(f"Length mismatch: Length of y_true ({len(y_true)}) and length of y_pred ({len(y_pred)}) are different")
+
+    if len(np.unique(y_true))>2 or len(np.unique(y_pred))>2:
+        raise ValueError("Unique classes count error: Only 2 unique classes must be present")
+
+    unq_classes = np.unique(y_true)
+    conf_mat = np.zeros((2,2))
+
+    for i in range(len(unq_classes)):
+
+        true_pos_sum = np.sum((y_true==y_pred)&(y_true==unq_classes[i]))
+        false_neg_sum = np.sum(y_true==unq_classes[i]) - true_pos_sum
+
+        conf_mat[i,i] = true_pos_sum
+        if i==0:
+            conf_mat[i,1] = false_neg_sum
+        else:
+            conf_mat[i,0] = false_neg_sum
+
+    return unq_classes,conf_mat
