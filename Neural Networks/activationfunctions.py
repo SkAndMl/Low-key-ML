31e516eb053f493aa92f695ce8c0ded991326253
Sathya Krishnan <86184014+SathyaKrishnan1211@users.noreply.github.com>
2022-04-10 07:32:46 +0530

    leakyrelu and elu

--- Neural Networks/activationfunctions.py
+++ Neural Networks/activationfunctions.py
@@ -4,7 +4,13 @@ def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def relu(x):
-    return np.max((0,x))
+    return np.maximum(0,x)
 
 def tanh(x):
     return 2*sigmoid(2*x)-1
+
+def leakyrelu(x,alpha=0.01):
+    return np.maximum(alpha*x,x)
+
+def elu(x,alpha=1):
+    return np.where(x<0,alpha*(np.exp(x)-1),x)
