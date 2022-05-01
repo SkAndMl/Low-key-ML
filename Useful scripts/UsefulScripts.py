fecd4e54eddb0621fc32c231d1082df773ce836f
sathya <mr.sk12112002@gmail.com>
2022-05-01 15:43:50 +0530

    log loss

--- Useful scripts/UsefulScripts.py
+++ Useful scripts/UsefulScripts.py
@@ -98,3 +98,12 @@ def gini_impurity(value_set):
   for j in value_set:
     gini -= (j/total_num)**2
   return np.round(gini,3)
+
+def log_loss(y_true,y_proba):
+    import numpy as np
+    avg_loss = 0
+    for i,j in zip(y_true,y_proba):
+        log_loss = -1*(i*np.log(j) + (1-i)*np.log(1-j))
+        avg_loss += log_loss
+
+    return avg_loss/len(y_true)
