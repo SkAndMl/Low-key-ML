0f361496ee706ed9b675974a77f06ca41926ad0b
SathyaKrishnan1211 <mr.sk12112002@gmail.com>
2022-05-02 06:37:16 +0530

    documentation update

--- Metrics/r2.py
+++ Metrics/r2.py
@@ -1,4 +1,9 @@
 def r2(y_true,y_pred):
+    """
+    y_true -> true labels
+    y_pred -> predicted labels
+    R2 defines how well does the model fit the data.
+    """
     import numpy as np
     mean_y_true = np.mean(y_true)
     num = np.sum(np.square(y_true-y_pred))
