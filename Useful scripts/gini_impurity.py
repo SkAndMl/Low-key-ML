3c30063e1f82a2f5075edb866d99675ee571f7c8
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2025-05-24 11:42:37 +0530

    refactor

--- Useful scripts/gini_impurity.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def gini_impurity(value_set):
-  # node impurity measurement for decision tree
-  total_num = np.sum(value_set)
-  gini = 1
-  for j in value_set:
-    gini -= (j/total_num)**2
-  return np.round(gini,3)
